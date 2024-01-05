28999853ab957eb8ae9d1d1854351a73eee04408
SAUD ALNASSER <saud4services@gmail.com>
2024-01-05 21:37:10 +0300

    feat: create rectangles project

---
+++ rectangles/src/main.rs
@@ -0,0 +1,28 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle::new(30, 50);
+    let rect2 = Rectangle::new(10, 40);
+    let rect3 = Rectangle::new(60, 45);
+
+    println!("can react1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can react1 hold rect3? {}", rect1.can_hold(&rect3));
+    println!("area of the rectangle is {} square pixels.", rect1.area());
+}
